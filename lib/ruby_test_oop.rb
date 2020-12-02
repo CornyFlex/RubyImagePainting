91e4bf10680cd46708a8fbef3c08e3f482896d6a
CornyFlex <spyrofireice@gmail.com>
2020-12-02 19:33:51 +0100

    Added exceptions, cleaned code, working.

--- lib/ruby_test_oop.rb
+++ lib/ruby_test_oop.rb
@@ -1,149 +1,144 @@
 # frozen_string_literal: true
 
 # This program is made for inputting commands and outputting the image as a matrix in an interactive environment
-
+#
 # The program only works with capital letters!
-
-# Initializing variable for storing the command name
-command_name = ''
 
 # Class for creating and editing the final image
 class Matrix
+
+  # Set maximum width and height of the matrix.
+  MAX_WIDTH = MAX_HEIGHT = 250
+
+  # Set default colour of the cells inside the matrix.
+  DEFAULT_COLOUR = 'O'
+
+  # Create a new matrix with given width and height values.
   def initialize(width, height)
-    @image = create_matrix(width, height)
+    raise ArgumentError, 'Invalid width' unless (0..MAX_WIDTH).include? width
+    raise ArgumentError, 'Invalid height' unless (0..MAX_HEIGHT).include? height
+
+    @image = Array.new(height) { Array.new(width, DEFAULT_COLOUR) }
   end
 
-  def valid_coordinates?(width, height, additional_width = 0, additional_height = 0)
-    true if width <= image_width && additional_width <= image_width && height <= image_height && additional_height <= image_height
+  # Checks for whether the entered coordinates are within bounds and not nil
+  def valid_coordinates?(x_coord, y_coord)
+    (0..width).include?(x_coord) && (0..height).include?(y_coord)
   end
 
-  # creates and returns matrix based on width and height
-  def create_matrix(width, height)
-    return unless width && height <= 250
+  # Return all cell values to default values
+  def clear
+    raise ArgumentError, 'Matrix does not exist, or is invalid.' if @image.nil?
 
-    Array.new(height) { Array.new(width) { 'O' } }
-  end
-
-  # return values in current matrix back to default
-  def clear_matrix
-    @image = Array.new(image_height) { Array.new(image_width) { 'O' } }
-  end
-
-  # display matrix
-  def display_matrix
-    @image.map { |value| puts value.join }
-  end
-  
-  # changes values (colour) of a specific field
-  def colour_field(x_coordinate, y_coordinate, colour)
-    return unless valid_coordinates?(x_coordinate, y_coordinate)
-
-    return if x_coordinate.nil? || y_coordinate.nil? || colour.nil?
-
-    set_colour(x_coordinate, y_coordinate, colour)
-    @image
-  end
-
-  # draws a vertical segment based on given input
-  def vertical_segment(column, first_row, second_row, colour)
-    return unless valid_coordinates?(column, first_row, 0, second_row)
-
-    return if column.nil? || first_row.nil? || second_row.nil? || colour.nil?
-
-    (first_row..second_row).each do |i|
-      set_colour(column, i, colour)
-    end
-    @image
-  end
-
-  # draws a horizontal segment based on input
-  def horizontal_segment(first_column, second_column, row, colour)
-    return unless valid_coordinates?(first_column, row, second_column, 0)
-
-    return if first_column.nil? || second_column.nil? || row.nil? || colour.nil?
-
-    (first_column..second_column).each do |j|
-      set_colour(j, row, colour)
-    end
-    @image
-  end
-
-  # fills in the matrix depending on the colour of a field
-  def fill_matrix(x_coordinate, y_coordinate, colour, memo=[])
-    return unless valid_coordinates?(x_coordinate, y_coordinate)
-
-    return if x_coordinate.nil? || y_coordinate.nil? || colour.nil?
-
-    memo.push([x_coordinate, y_coordinate]) # add us to the memo
-
-    (x_coordinate - 1..x_coordinate + 1).each do |i|
-      (y_coordinate - 1..y_coordinate + 1).each do |j|
-        next if memo.include? [i, j]
-
-        if get_colour(i, j) == get_colour(x_coordinate, y_coordinate)
-          fill_matrix(i, j, colour, memo)
-          set_colour(i, j, colour)
-        end
+    width.times do |i|
+      height.times do |j|
+        self[i, j] = DEFAULT_COLOUR
       end
     end
-    set_colour(x_coordinate, y_coordinate, colour)
   end
 
-  def image_height
+  # Display the current state of the matrix
+  def to_s
+    @image.map(&:join).join "\n"
+  end
+
+  # Draws a vertical line inside of the matrix, based on colour
+  def vertical_line(column, first_row, second_row, colour)
+    raise ArgumentError, 'Invalid starting position.' unless valid_coordinates?(first_row, column)
+    raise ArgumentError, 'Invalid end position.' unless valid_coordinates?(second_row, column)
+    raise ArgumentError, 'Colour not set.' unless colour
+
+    (first_row..second_row).each do |i|
+      self[column, i] = colour
+    end
+  end
+
+  # Draws a horizontal line inside of the matrix, based on colour
+  def horizontal_line(first_column, second_column, row, colour)
+    raise ArgumentError, 'Invalid starting position.' unless valid_coordinates?(row, first_column)
+    raise ArgumentError, 'Invalid end position.' unless valid_coordinates?(row, second_column)
+    raise ArgumentError, 'Colour not set.' unless colour
+
+    (first_column..second_column).each do |j|
+      self[j, row] = colour
+    end
+  end
+
+  # Changes values of neighbouring cells based on the cell with value [row, column], with a colour of choice
+  def fill(row, column, colour, memo = [])
+    start_colour = self[row, column]
+    self[row, column] = colour
+
+    memo.push([row, column]) # adds coordinates to memo
+
+    (row - 1..row + 1).each do |i|
+      (column - 1..column + 1).each do |j|
+        next unless valid_coordinates?(i, j)
+        next if memo.include? [i, j]
+        next unless self[i, j] == start_colour
+
+        fill(i, j, colour, memo)
+      end
+    end
+  end
+
+  # Returns the height of the matrix
+  def height
     @image.length
   end
 
-  def image_width
+  # Returns the width of the matrix
+  def width
     @image[0].length
   end
 
-  def get_colour(x_coordinate, y_coordinate)
+  # Returns the value of a cell inside of the matrix
+  def [](x_coordinate, y_coordinate)
+    raise ArgumentError, 'Invalid coordinates' unless valid_coordinates?(x_coordinate, y_coordinate)
+
     @image[y_coordinate - 1][x_coordinate - 1]
   end
 
-  def set_colour(x_coordinate, y_coordinate, colour)
+  # Sets the value of a cell inside of the matrix
+  def []=(x_coordinate, y_coordinate, colour)
+    raise ArgumentError, 'Invalid coordinates!' unless valid_coordinates?(x_coordinate, y_coordinate)
+    raise ArgumentError, 'Missing colour.' unless colour
+
     @image[y_coordinate - 1][x_coordinate - 1] = colour
   end
 
-  private :image_height, :image_width, :get_colour, :set_colour
+  private :height, :width
 end
 
+# Initialize matrix with nil
+image = nil
+
 # loop used for checking whether user wants to exit the program
-while command_name != 'X'
-
+loop do
   # getting user input and storing values into variables
-  command_name, first_variable, second_variable, third_variable, fourth_variable = gets.split
+  command, *args = gets.split
 
-  case command_name # checking for commands
-  when 'I' # used for creating a matrix
-    width = first_variable.to_i
-    height = second_variable.to_i
+  if command != 'I' && !image
+    warn 'Initialize matrix first.'
+    next
+  end
 
-    image = Matrix.new(width, height)
-    puts 'Error creating matrix' if image.nil?
-  when 'C' # used for clearing out any values inside of the matrix
-    puts 'Error clearing out the matrix.' if image&.clear_matrix.nil?
-  when 'S' # displays current state of the matrix
-    puts 'Error displaying the matrix.' if image&.display_matrix.nil?
-  when 'L' # used for changing the value (colour) of a single field in the matrix
-    if image&.colour_field(first_variable.to_i, second_variable.to_i, third_variable).nil?
-      puts 'Error changing the value of a field in the matrix.'
+  args[0] &&= args[0].to_i
+  args[1] &&= args[1].to_i
+
+  begin
+    case command # checking for commands
+    when 'I' then image = Matrix.new(args[0], args[1])
+    when 'C' then image.clear
+    when 'S' then puts image
+    when 'L' then image[args[0], args[1]] = args[2]
+    when 'V' then image.vertical_line(args[0], args[1], args[2].to_i, args[3])
+    when 'H' then image.horizontal_line(args[0], args[1], args[2].to_i, args[3])
+    when 'F' then image.fill(args[0], args[1], args[2])
+    when 'X' then break
+    else warn "Unable to perform '#{command}' command. Try again."
     end
-  when 'V' # used for drawing a vertical line in the matrix
-    if image&.vertical_segment(first_variable.to_i, second_variable.to_i, third_variable.to_i, fourth_variable).nil?
-      puts 'Error drawing a vertical line in the matrix.'
-    end
-  when 'H' # used for drawing a horizontal line in the matrix
-    if image&.horizontal_segment(first_variable.to_i, second_variable.to_i, third_variable.to_i, fourth_variable).nil?
-      puts 'Error drawing a horizontal line in the matrix.'
-    end
-  when 'F' # used for filling in the matrix depending on the colour of a field
-    if image&.fill_matrix(first_variable.to_i, second_variable.to_i, third_variable).nil?
-      puts 'Error filling in the matrix.'
-    end
-  when 'X' # used for exiting the program
-    break
-  else
-    puts 'Error, wrong command name. Try again.'
+  rescue StandardError => e
+    warn "Unable to perform '#{command}': #{e}"
   end
 end
